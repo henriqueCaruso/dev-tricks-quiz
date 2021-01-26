edbf2117332f65a75dfa381b388bba9ebfa38ad6
ccaruso <henrique99caruso@gmail.com>
2021-01-25 23:59:39 -0200

    Primeira Aula da Imersão Next.js

--- pages/quiz.js
+++ pages/quiz.js
@@ -25,32 +25,22 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>{db.title}</h1>
-          </Widget.Header>
-          <Widget.Content>
-            <p>{db.description}</p>
-          </Widget.Content>
-          <a href="quiz">
-            Vamos Jogar!
-          </a>
-        </Widget>
+    return (
+        <QuizBackground backgroundImage={db.bg}>
+            <QuizContainer>
+                <QuizLogo />
+                <Widget>
+                    <Widget.Header>
+                        <h1>DESAFIO DE NAVEGAÇÃO FEITO!</h1>
+                    </Widget.Header>
+                    <Widget.Content>
+                        <p>{db.description}</p>
+                    </Widget.Content>
+                </Widget>
 
-        <Widget>
-          <Widget.Content>
-            <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
-    </QuizBackground>
-  );
+            </QuizContainer>
+            <GitHubCorner projectUrl="https://github.com/omariosouto" />
+        </QuizBackground>
+    );
 }
